2ccce5105e7372b0b61757b36af068b174558196
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-29 16:36:01 -0400

    continued

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const express = require('express');
-
+const ExpressError = require('./expressError')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/mean', (req, res) => {
-    return res.send("hello")
+    if (!req.query.nums) {
+        throw new ExpressError("Numbers are required", 400)
+    }
+
+    let numsQuery = req.query.nums;
+    let nums = numsQuery.split(',');
+    let errors = []
+
+    nums.forEach((num) => {
+        if (Number(num) == NaN) {
+            errors.push(num)
+        }
+    })
+
+
+
+    let mean = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        mean += nums[i]
+    }
+
+    let valueNum = mean / nums.length
+
+    return res.json(
+        {
+            response: {
+                operation: "mean",
+                value: valueNum
+            }
+        })
 });
 
 
