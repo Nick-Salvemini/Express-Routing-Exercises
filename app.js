47a11d4f5548ae428506e2e47e3cfe9dc114ef39
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-30 12:42:34 -0400

    update

--- app.js
+++ app.js
@@ -4,23 +4,48 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    next()
+})
 
-app.get('/mean', (req, res) => {
-    if (!req.query.nums) {
-        throw new ExpressError("Numbers are required", 400)
+app.get('/mean', (req, res, next) => {
+    try {
+        if (!req.query.nums) {
+            throw new ExpressError("Numbers are required", 400)
+        }
+    } catch (error) {
+        next(error)
     }
 
     let numsQuery = req.query.nums;
     let nums = numsQuery.split(',');
-    let errors = []
+    let errors = ''
 
     nums.forEach((num) => {
+        // console.log(typeof (num), Number(num))
         if (Number(num) == NaN) {
-            errors.push(num)
+            // console.log(num)
+            if (errors == '') {
+                errors += num
+            } else {
+                errors += ', and ';
+                errors += num
+            }
         }
+        // console.log('xxxxxxxxxx', errors)
     })
 
+    // console.log('********', numsQuery)
+    // console.log('!!!!!!!!!!', nums)
+    // console.log('xxxxxxxxxx', errors)
 
+    try {
+        if (errors.length != 0) {
+            throw new ExpressError("Numbers are required", 400)
+        }
+    } catch (error) {
+        next(error)
+    }
 
     let mean = 0;
 
@@ -39,6 +64,9 @@ app.get('/mean', (req, res) => {
         })
 });
 
+app.use((error, req, res, next) => {
+    res.status(error.status).send(error.msg)
+})
 
 /** Start server on port 3000 */
 app.listen(3000, function () {
